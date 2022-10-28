7ff4f0dedeb5b1f965ef6083ae9d1b9de9d2766d
RonTuretzkyMigal <turetzkyron@gmail.com>
2022-10-28 13:58:16 +0300

    changing setup to migrate db and make other changes plus removing bad.py

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
+#!/bin/bash -ex
 sudo -n dnf install -y pipenv 
 cd /vagrant 
 pipenv sync --dev 
-(setsid pipenv run \ 
-	python manage.py runserver 0.0.0.0:8000 > runserver.log 2>&1 &) 
+pipenv run python manage.py migrate
+nohup pipenv run python manage.py runserver 0.0.0.0:8000 & 
